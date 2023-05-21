f9cccbd4309946d4d349a0e0db940f5942fdd56c
guowuchen <100406756+guowuchen@users.noreply.github.com>
2023-05-21 14:04:20 +0800

    Update detect.py
    Signed-off-by: guowuchen <100406756+guowuchen@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -43,9 +43,10 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams#loadimages图片或视频加载；loadscreenshot屏幕加载；loadstream网络流加载
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+#LOGGER日志打印 profile时间处理 check_file文件检查 check_img_size图片尺寸检查 check_imshow图像展示检查
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
 
@@ -80,45 +81,45 @@ def run(
         dnn=False,  # use OpenCV DNN for ONNX inference
         vid_stride=1,  # video frame-rate stride
 ):
-    source = str(source)
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
-    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
-    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-    webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
-    screenshot = source.lower().startswith('screen')
+    source = str(source)#转换source为字符串
+    save_img = not nosave and not source.endswith('.txt')  # save inference images，保存推理图片；若nosave传入false，且source不是以txt结尾，save_img=True
+    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)#suffix函数用于提取后缀，判断后缀名字是否在IMG_FORMATS+VID_FORMATS列表中，如果存在此目录中，则返回True
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))#lower转小写，startswith检查字符串是否以指定字符串开头，是返回True，否则返回false
+    webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)#web通过3个或，判断是否是摄像头，若是，webcam=True
+    screenshot = source.lower().startswith('screen')#screenshot判断是否是当前屏幕
     if is_url and is_file:
-        source = check_file(source)  # download
+        source = check_file(source)  # download，若is_url和is_file同时成立，则通过check_file进行下载相应文件
 
     # Directories
     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
-    device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
-    stride, names, pt = model.stride, model.names, model.pt
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    device = select_device(device)#根据device参数，选择cpu或者gpu
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)#判断不同深度学习框架
+    stride, names, pt = model.stride, model.names, model.pt#模型步长，模型类别名，模型pt文件
+    imgsz = check_img_size(imgsz, s=stride)  # check image size，检测图片尺寸
 
     # Dataloader
-    bs = 1  # batch_size
-    if webcam:
+    bs = 1  # batch_size，每次推理一张图片
+    if webcam:#判断是否为摄像头检测
         view_img = check_imshow(warn=True)
         dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
         bs = len(dataset)
-    elif screenshot:
+    elif screenshot:#判断是否为屏幕检测
         dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
-    else:
+    else:#加载图片文件，在LoadImages中，主要是对source进行操作，若是文件夹，则会遍历文件夹中所有图片存入dataset中
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
     vid_path, vid_writer = [None] * bs, [None] * bs
 
-    # Run inference
+    # Run inference，进行推理
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
-    for path, im, im0s, vid_cap, s in dataset:
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())#seen=0(计数,后续通过+1，处理多少图片）；windows空列表，用于后续处理显示图片；dt表示耗时记录
+    for path, im, im0s, vid_cap, s in dataset:#dataset通过LoadImages加载而来，loadimages中存储了path（路径信息），im（经尺寸处理后的图片），im0（原图），vid_cap(视频参数），s是打印信息
         with dt[0]:
-            im = torch.from_numpy(im).to(model.device)
-            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-            im /= 255  # 0 - 255 to 0.0 - 1.0
+            im = torch.from_numpy(im).to(model.device)#torch.from_numpy将图片转换成torch支持的格式，放入模型device中
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32，判断是否为半精度
+            im /= 255  # 0 - 255 to 0.0 - 1.0，将图片所有像素点归一化
             if len(im.shape) == 3:
                 im = im[None]  # expand for batch dim
 
@@ -126,17 +127,18 @@ def run(
         with dt[1]:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(im, augment=augment, visualize=visualize)
+            #推理阶段，得到所有检测框pred信息 torch.size=[1,18900,85]
 
         # NMS
-        with dt[2]:
+        with dt[2]:#非极大值抑制，返回pred；最终保留了5个检测框，每个检测框包含6个信息[xmin,ymin,xmax,ymax,置信度p，类别c]
             pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
         # Second-stage classifier (optional)
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
 
         # Process predictions
-        for i, det in enumerate(pred):  # per image
-            seen += 1
+        for i, det in enumerate(pred):  # per image,i是每个batch的信息，处理到第几个物体；det代表每个检测框的信息，在这张图片中有5个检测框，且有6个信息
+            seen += 1#计数，来遍历所有图片
             if webcam:  # batch_size >= 1
                 p, im0, frame = path[i], im0s[i].copy(), dataset.count
                 s += f'{i}: '
@@ -144,10 +146,10 @@ def run(
                 p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
-            s += '%gx%g ' % im.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            save_path = str(save_dir / p.name)  # im.jpg，检测图片存储路径
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt，坐标txt存储路径
+            s += '%gx%g ' % im.shape[2:]  # print string，图片尺寸信息
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh，原图片宽和高信息
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
             if len(det):
